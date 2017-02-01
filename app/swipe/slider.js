117a8589b2c7e722681cc20d83b335ab1194daf4
Dynodzzo <dynodzoo@gmail.com>
2017-02-01 19:12:01 +0100

    Slideshow config
    Updated transition effect

--- app/swipe/slider.js
+++ app/swipe/slider.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 	$('.main-slider').slick({
 		arrows: false,
-		dots: true
+		useTransform: true,
+		draggable: true,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		cssEase: 'cubic-bezier(0.230, 1.000, 0.320, 1.000)',
+		speed: 1200
 	});
 });
